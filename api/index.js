249f233c787f05b2f4cc67d54f08aceddc38dfad
janithachathuni <janithachathuni@gmail.com>
2025-08-27 05:51:11 +0530

    connected mongoose

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors')
@@ -10,97 +11,98 @@ app.use(express.json());
 //mongodb password nbLE51snak0wtG57
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-}
-)
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// }
+// )
 
-//mongodb configuration
+// //mongodb configuration
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://birder-application:nbLE51snak0wtG57@cluster0.tahbmnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const uri = "mongodb+srv://birder-application:nbLE51snak0wtG57@cluster0.tahbmnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
 
-    //create a collection of documents
-    const birdCollections = client.db("KurulloDatabase").collection("birds");
+//     //create a collection of documents
+//     const birdCollections = client.db("KurulloDatabase").collection("birds");
 
-    //insert a bird to the database using the db post method
-    app.post("/upload-bird", async(req, res) => {
-        const data = req.body;
-        const result = await birdCollections.insertOne(data);
-        res.send(result);
-    })
+//     //insert a bird to the database using the db post method
+//     app.post("/upload-bird", async(req, res) => {
+//         const data = req.body;
+//         const result = await birdCollections.insertOne(data);
+//         res.send(result);
+//     })
 
-    //get all birds from database
-    app.get("/all-birds", async(req, res) =>{
-        const birds = birdCollections.find();
-        const result = await birds.toArray();
-        res.send(result);
-    })
+//     //get all birds from database
+//     app.get("/all-birds", async(req, res) =>{
+//         const birds = birdCollections.find();
+//         const result = await birds.toArray();
+//         res.send(result);
+//     })
 
-    //update a bird data patch or update method
-    app.patch("/bird/:id", async(req, res)=>{
-        const id = req.params.id;
-        // console.log(id);
-        const updateBirdData = req.body;
+//     //update a bird data patch or update method
+//     app.patch("/bird/:id", async(req, res)=>{
+//         const id = req.params.id;
+//         // console.log(id);
+//         const updateBirdData = req.body;
 
-        const filter = {_id:new ObjectId(id)};
-        const options = {upsert: true};
+//         const filter = {_id:new ObjectId(id)};
+//         const options = {upsert: true};
 
-        const updateDoc = {
-            $set:{
-                ...updateBirdData
-            }
-        }
+//         const updateDoc = {
+//             $set:{
+//                 ...updateBirdData
+//             }
+//         }
 
-        //update
-        const result = await birdCollections.updateOne(filter, updateDoc, options);
-        res.send(result);
-    })
+//         //update
+//         const result = await birdCollections.updateOne(filter, updateDoc, options);
+//         res.send(result);
+//     })
 
-    //delete a bird data
-    app.delete("book/:id", async(req, res) => {
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const result = await birdCollections.deleteOne(filter);
-        res.send(result);
-    })
+//     //delete a bird data
+//     app.delete("book/:id", async(req, res) => {
+//         const id = req.params.id;
+//         const filter = {_id: new ObjectId(id)};
+//         const result = await birdCollections.deleteOne(filter);
+//         res.send(result);
+//     })
 
-    //finding a bird by family (filtering out data)
-    app.get("/all-birds", async(req, res) =>{
-      let query = {};
-      if(req.query?.family){
-        query = {family:req.query.family}
-      }
+//     //finding a bird by family (filtering out data)
+//     app.get("/all-birds", async(req, res) =>{
+//       let query = {};
+//       if(req.query?.family){
+//         query = {family:req.query.family}
+//       }
 
-      const result = await birdCollections.find(query).toArray();
-      res.send(result);
-    })
+//       const result = await birdCollections.find(query).toArray();
+//       res.send(result);
+//     })
 
     
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  //   await client.db("admin").command({ ping: 1 });
+  //   console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  // } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
-  }
-}
-run().catch(console.dir);
+//   }
+// }
+// run().catch(console.dir);
+
 
 
 app.listen(port, () => {
